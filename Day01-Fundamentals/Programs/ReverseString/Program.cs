3019bbaf5e554286081a94d9a4b6184798ffbfa0
shindesushma3100 <shindesushma3100@gmail.com>
2026-07-24 17:57:17 +0530

    Add Reverse String program

---
+++ Day01-Fundamentals/Programs/ReverseString/Program.cs
@@ -0,0 +1,18 @@
+﻿// Console.Write("Enter a String :");
+// string input = Console.ReadLine();
+
+// char[] chars = input.ToCharArray();
+// Array.Reverse(chars);
+// string reversed =new  string(chars);
+
+// Console.WriteLine($"Reversed String:{reversed}");
+
+
+Console.Write("Enter String :");
+string input= Console.ReadLine();
+
+char[] chars= input.ToCharArray();
+Array.Reverse(chars);
+
+string reversed =new  string(chars);
+Console.WriteLine($"Reversed String :{reversed}");
