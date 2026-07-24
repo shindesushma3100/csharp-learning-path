ca7081a5f26960879955a5a3bdb92c27573b4986
shindesushma3100 <shindesushma3100@gmail.com>
2026-07-24 18:35:12 +0530

    Add .getignore, update ReverseString with LInQ approch

--- Day01-Fundamentals/Programs/ReverseString/Program.cs
+++ Day01-Fundamentals/Programs/ReverseString/Program.cs
@@ -7,7 +7,7 @@
 
 // Console.WriteLine($"Reversed String:{reversed}");
 
-
+//Array Reverse
 Console.Write("Enter String :");
 string input= Console.ReadLine();
 
@@ -16,3 +16,12 @@ Array.Reverse(chars);
 
 string reversed =new  string(chars);
 Console.WriteLine($"Reversed String :{reversed}");
+
+//String Reverse
+
+Console.Write("Enter 2nd String :");
+string input2 = Console.ReadLine();
+string stringarr = input2.ToString();
+string temp = new  string(stringarr.Reverse().ToArray());
+Console.WriteLine(temp);
+
