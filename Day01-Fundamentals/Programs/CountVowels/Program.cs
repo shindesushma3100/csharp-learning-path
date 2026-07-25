06e300070255d6f690728690726a022cdf4f75be
shindesushma3100 <shindesushma3100@gmail.com>
2026-07-25 12:46:33 +0530

    add count word in same code

--- Day01-Fundamentals/Programs/CountVowels/Program.cs
+++ Day01-Fundamentals/Programs/CountVowels/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.Write("Enter a string: ");
 string input=Console.ReadLine();
+
+//Count vowels
 string vowles="aieou";
 int count=0;
 foreach(char c in input)
@@ -10,3 +12,7 @@ foreach(char c in input)
     }
 }
 Console.WriteLine($"Number of vowels: {count}");
+//Count word
+
+int cout2 =input.ToString().Length;
+Console.WriteLine($"Number of count of {input} is {cout2}");
