e6c4afcf07b0f501d827b95fbe5ea67263112034
shindesushma3100 <shindesushma3100@gmail.com>
2026-07-25 10:44:50 +0530

    Added Factorial Program

---
+++ Day01-Fundamentals/Programs/Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter a number:");
+int number =int.Parse(Console.ReadLine());
+
+long factorial = 1;
+for(int i = 1; i <= number; i++)
+{
+    factorial *=i;
+}
+Console.WriteLine($"Factorial of {number} is {factorial}");
+// ``````````````````````````````````````
+Console.Write("Enter a number:");
+int number1=int.Parse(Console.ReadLine());
+int fact=1;
+for(int i = 1; i <= number1; i++)
+{
+    fact *=i;
+
+}
+
+Console.WriteLine($"Factorial of {number1} is {fact}");
