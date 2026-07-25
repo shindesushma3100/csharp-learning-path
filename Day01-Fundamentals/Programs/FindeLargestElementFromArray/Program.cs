8d7a44f4cba780cf8123fa5bee185e34905d8520
shindesushma3100 <shindesushma3100@gmail.com>
2026-07-25 15:36:04 +0530

    added second largest numver in array for same code

--- Day01-Fundamentals/Programs/FindeLargestElementFromArray/Program.cs
+++ Day01-Fundamentals/Programs/FindeLargestElementFromArray/Program.cs
@@ -4,7 +4,7 @@
 // Walk through every other element — whenever you find something bigger, update largest
 // By the end, you've compared every element and largest holds the true maximum
 // (Side note: C# also has a built-in numbers.Max() that does this in one line — good to know for real projects, but writing the loop yourself first builds the underlying intuition)
-
+//Largest Number
 int[] numbers={45,12,89,33,67};
 int largest = numbers[0];
 foreach(int num in numbers)
@@ -16,3 +16,23 @@ foreach(int num in numbers)
 }
 Console.WriteLine("Array: "+ string.Join(", ", numbers));
 Console.WriteLine($"Largest number: {largest}");
+
+//Second Largest
+int[] numbers1 = { 45, 12, 89, 33, 67 };
+int largest2 =int.MinValue;
+int secondLargest= int.MinValue;
+foreach(int num in numbers1)
+{
+    if(num > largest2)
+    {
+        secondLargest= largest2;
+        largest2 = num;
+    }
+    else if(num > secondLargest && num != largest2)
+    {
+        secondLargest = num;
+    }
+
+}
+Console.WriteLine("Array: "+ string.Join(", ", numbers1));
+Console.WriteLine($"Largest:{largest2}, Second Largest: {secondLargest}");
