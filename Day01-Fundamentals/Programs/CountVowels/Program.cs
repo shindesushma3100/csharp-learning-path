cd13c3f2c30ce5081d3f5104af2f1c214484c8ca
shindesushma3100 <shindesushma3100@gmail.com>
2026-07-25 12:40:17 +0530

    Add Count Vowels in string program

---
+++ Day01-Fundamentals/Programs/CountVowels/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a string: ");
+string input=Console.ReadLine();
+string vowles="aieou";
+int count=0;
+foreach(char c in input)
+{
+    if (vowles.Contains(c))
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Number of vowels: {count}");
