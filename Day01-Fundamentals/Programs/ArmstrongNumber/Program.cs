952fd11705cd5ac036b77402d5ab32d48e43fa80
shindesushma3100 <shindesushma3100@gmail.com>
2026-07-25 12:07:03 +0530

    Add ArmStrong  Number Program

---
+++ Day01-Fundamentals/Programs/ArmstrongNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿// Key logic:
+
+// number % 10 extracts the last digit (e.g., 153 % 10 = 3)
+// number / 10 chops off that last digit using integer division (153 / 10 = 15)
+// We repeat this until number becomes 0, meaning we've processed every digit
+// Math.Pow(digit, digitCount) raises each digit to the power of the total digit count, and we sum them all
+
+using System.ComponentModel;
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+int original = number;
+int digitCount = number.ToString().Length;
+int sum=0;
+
+while (number > 0){
+    int digit =number %10;
+    sum += (int)Math.Pow(digit,digitCount);
+    number = number/10;
+}
+if (sum == original)
+{
+    Console.WriteLine($"{original} is Armstrong Number");
+}
+else
+{
+    Console.WriteLine($"{original}is NOT an Armstrong Number");
+}
