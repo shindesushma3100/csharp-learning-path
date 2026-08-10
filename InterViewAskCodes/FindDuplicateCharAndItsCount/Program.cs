393888185cb7f7f03225ffac9e37a632d7811c68
shindesushma3100 <shindesushma3100@gmail.com>
2026-08-10 17:45:49 +0530

    Add Find Duplicate Char from String Using LINQ Program

---
+++ InterViewAskCodes/FindDuplicateCharAndItsCount/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Linq;
+
+// Console.Write("Enter Name :");
+string name= "Sushma Shinde".ToLower().Replace(" ","");
+
+var duplicate = name
+                .GroupBy(c => c)
+                .Where(g => g.Count()> 1);
+
+foreach(var c in duplicate)
+{
+    Console.WriteLine($"{c.Key} = {c.Count()}");
+}
